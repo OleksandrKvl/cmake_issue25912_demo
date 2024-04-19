20c4608d6b4e84428b957c55276a487f2602c9a1
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2024-04-19 14:06:38 +0300

    first commit

---
+++ consumer.cpp
@@ -0,0 +1,6 @@
+#include <generated_file.h>
+
+int main()
+{
+    return 0;
+}
---
+++ schema_compiler.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main(int argc, char** argv)
+{
+    if(argc != 2)
+    {
+        std::cerr << "Wrong number of arguments\n";
+        return 1;
+    }
+
+    const auto path = argv[1];
+    std::ifstream file(path);
+    if(!file.is_open())
+    {
+        std::cerr << "Unable to open file: " << path << '\n';
+        return 1;
+    }
+
+    // create a file for each line
+    std::string output_file_name;
+    while(std::getline(file, output_file_name))
+    {
+        std::ofstream output_file(output_file_name);
+        if(!output_file.is_open())
+        {
+            std::cerr << "Unable to create file: " << output_file_name << '\n';
+            return 1;
+        }
+
+        output_file.close();
+    }
+
+    file.close();
+
+    return 0;
+}
